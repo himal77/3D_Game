6c5805fd31e0f3963c4eda6c4f683d3f566a434f
H <himalpuri6@gmail.com>
2019-09-10 13:20:11 +0200

    Matrix transformation

--- 3D_Game/src/shaders/StaticShader.java
+++ 3D_Game/src/shaders/StaticShader.java
@@ -1,10 +1,13 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
 
 	private static final String vertex_file = "src/shaders/vertexShader.txt";
 	private static final String fragment_file = "src/shaders/fragmentShader.txt";
 	
+	private int loaction_transformationMatrix;
 	
 	public StaticShader() {
 		super(vertex_file, fragment_file);
@@ -15,4 +18,13 @@ public class StaticShader extends ShaderProgram{
 		super.bindAttributes(1, "texture_coords");
 	}
 
+	
+	protected void getAlluniformLocations() {
+		super.getUniformLocation("transformationMatrix");
+		
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(loaction_transformationMatrix, matrix);
+	}
 }
