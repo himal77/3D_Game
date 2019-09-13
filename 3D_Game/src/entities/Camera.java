9aaff4c8be4e27486779926e8ff190b43a9bef6f
H <himalpuri6@gmail.com>
2019-09-13 10:42:44 +0200

    Object Loader with .obj file

--- 3D_Game/src/entities/Camera.java
+++ 3D_Game/src/entities/Camera.java
@@ -4,42 +4,44 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
-	
-		private Vector3f position = new Vector3f(0, 0, 0);
-		private float pitch;
-		private float yaw;
-		private float roll;
-		
-		public Camera() {}
-		
-		public void move() {
-			if(Keyboard.isKeyDown(Keyboard.KEY_W)) {
-				position.z -= 0.02f;
-			}
-			if(Keyboard.isKeyDown(Keyboard.KEY_D)) {
-				position.x += 0.02f;
-			}
-			if(Keyboard.isKeyDown(Keyboard.KEY_A)) {
-				position.x -= 0.02f;
-			}
-		}
 
-		public Vector3f getPosition() {
-			return position;
-		}
+	private Vector3f position = new Vector3f(0, 0, 0);
+	private float pitch;
+	private float yaw;
+	private float roll;
 
-		public float getPitch() {
-			return pitch;
-		}
+	public Camera() {
+	}
 
-		public float getYaw() {
-			return yaw;
+	public void move() {
+		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			position.z -= 0.2f;
 		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			position.x += 0.2f;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			position.x -= 0.2f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			position.z += 0.2f;
+		}
+	}
 
-		public float getRoll() {
-			return roll;
-		}
-		
-		
-		
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+
 }
